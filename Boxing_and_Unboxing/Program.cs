938f9089764da11983c45dc44c13bb9a80ad1701
Soumay kanti Das <anikds2859@outlook.com>
2021-05-25 19:05:51 +0600

    Add project files.

---
+++ Boxing_and_Unboxing/Program.cs
@@ -0,0 +1,26 @@
+﻿//Boxing & in boxing
+using System;
+//int i = 123;
+//object o = (object) i;// boxing
+//o = 123
+//i = (int) 0; // unboxing
+class testUnboxing
+{
+    static void Main()
+    {
+        int i = 123;
+        object o = i // implicit boxing
+    }
+    try
+    {
+     int j = (short) o;  
+       //int j = int o;             // attempt to unbox
+    Console.WriteLine("Unboxing ok.");
+
+     }
+    catch (InvalidCastException e)
+    {
+    Console.WriteLine("{0} Error: Incorrect unboxing.", e.Message);
+    }
+  }
+}
